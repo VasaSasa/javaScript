981dad12a89d9831f1e55063b7adc7a0c3b4e3ca
Václav Doležal <bc.va.do@gmail.com>
2022-03-01 19:54:53 +0100

    Vytvoření html,js,txt

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+// alert('Výtám tě na mojí stránce :-)');
+
+
+
+
+
+console.log("Tohle bude v konzoli......");
+
+
+
+
+console.log('Umíme s konzolí!!!!');
+
+let zvire = prompt('Vaše oblíbené zvíře???:');
+
+
+console.log('Tvoje oblíbené zvířeje:');
+console.log(zvire);
+
+
+console.log("zvire")
+
+
+let cislo = 5;
+let nove_cislo = 6;
+
